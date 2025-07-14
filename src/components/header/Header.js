ee3224ebc3465304f27490be5166afd65cba6412
MDShakibul <shakib6610@gmail.com>
2025-07-15 02:10:28 +0600

    bug solve for navber

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -85,7 +85,7 @@ const Header = () => {
 							</div>
 						</div>
 						<div className="col-lg-6 col-2">
-							<nav className="main_menu navbar navbar-expand-lg" style={{ display: mobailActive ? 'none' : '' }}>
+							<nav className="main_menu navbar navbar-expand-lg" style={{ display: !mobailActive ? 'none' : '' }}>
 								<div
 									className={`main_menu_inner collapse navbar-collapse justify-content-center ${
 										mobailActive ? 'show' : ''
